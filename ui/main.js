379079567cd831757780b2ec7437390fbfa16c39
efren-corillo <efren.corillo@doublybear.dev>
2025-11-01 14:41:43 +0800

    Add initial Quasar UI setup with configuration, components, and deployment files

--- ui/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp, h } from 'vue'
-import { Quasar } from 'quasar'
-import 'quasar/src/css/index.sass'
-
-const App = { render: () => h('div', 'Hello Quasar!') }
-createApp(App).use(Quasar, {}).mount('#q-app')
